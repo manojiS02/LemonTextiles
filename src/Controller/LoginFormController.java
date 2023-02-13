cffd969e3da6e2decb80628d15c8812b3ce18d14
manojiS02 <salakshanamanoji@gmail.com>
2023-02-13 13:11:33 +0530

    Added initial Project

---
+++ src/Controller/LoginFormController.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+
+public class LoginFormController {
+    public void OnActionPassword(ActionEvent actionEvent) {
+    }
+
+    public void OnActionLogin(ActionEvent actionEvent) {
+    }
+}
